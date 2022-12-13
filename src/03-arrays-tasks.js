c3805dadc8bf8fe6d07c63772418f384e4a1bc6a
Sergei Kudlai <s.kudlai@mail.ru>
2022-12-13 14:08:29 +0300

    feat:completed task array sortDigitNamesByNumericOrder

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -335,8 +335,13 @@ function getPositivesCount(arr) {
  *   [ 'nine','eight','nine','eight'] => [ 'eight','eight','nine','nine']
  *   [ 'one','one','one','zero' ]     => [ 'zero','one','one','one' ]
  */
-function sortDigitNamesByNumericOrder(/* arr */) {
-  throw new Error('Not implemented');
+function sortDigitNamesByNumericOrder(arr) {
+  const ARR_START = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+
+  return arr
+    .map((value) => ARR_START.findIndex((elem) => elem === value))
+    .sort((a, b) => a - b)
+    .map((value) => ARR_START[value]);
 }
 
 /**
